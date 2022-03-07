a70701731724608600d5e5b3ca7ed9181c203648
Andre Gonzales <gibstock@gmail.com>
2022-03-07 11:38:08 -0800

    cache version change

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const cacheName = 'site-static-v23-19' // need to update version with changes
-const dynamicCacheName = 'site-dynamic-v6-11' // need to update version with changes
+const cacheName = 'site-static-v23-20' // need to update version with changes
+const dynamicCacheName = 'site-dynamic-v6-12' // need to update version with changes
 const assets = [
   '/',
   '/index.html',
