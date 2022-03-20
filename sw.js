a564484088ac349aba3f99e8a1c96858112470ed
Andre Gonzales <gibstock@gmail.com>
2022-03-19 18:20:03 -0700

    added sound file support for the rest of the locations

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const cacheName = 'site-static-v23-30' // need to update version with changes
-const dynamicCacheName = 'site-dynamic-v6-22' // need to update version with changes
+const cacheName = 'site-static-v23-31' // need to update version with changes
+const dynamicCacheName = 'site-dynamic-v6-23' // need to update version with changes
 const assets = [
   '/',
   '/index.html',
